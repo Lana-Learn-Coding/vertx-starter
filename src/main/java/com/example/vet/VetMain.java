7d9e2b2b8f06255e40bea930febfa9712137ce1a
lana <sitdownrightnow.dev@gmail.com>
2020-06-08 11:46:29 +0700

    run multiple http server

--- src/main/java/com/example/vet/VetMain.java
+++ src/main/java/com/example/vet/VetMain.java
@@ -3,6 +3,7 @@ package com.example.vet;
 import com.example.vet.database.VetDatabase;
 import com.example.vet.http.VetHttpServer;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 
 public class VetMain extends AbstractVerticle {
@@ -12,10 +13,12 @@ public class VetMain extends AbstractVerticle {
         vertx.deployVerticle(new VetDatabase(), vetDBDeployment);
         vetDBDeployment.future().compose(id -> {
             Promise<String> vetHttpDeployment = Promise.promise();
-            vertx.deployVerticle(new VetHttpServer(), vetHttpDeployment);
+            // Don't use new (pass instance) if you want to specify the number of instances
+            // Because Vertx can't magically turn one instance of a Java object into N instances
+            vertx.deployVerticle(VetHttpServer.class, new DeploymentOptions().setInstances(2), vetHttpDeployment);
             return vetHttpDeployment.future();
         })
                 .onComplete(ar -> promise.complete())
-                .onFailure(throwable -> promise.fail(throwable.getCause()));
+                .onFailure(ar -> promise.fail(ar.getCause()));
     }
 }
