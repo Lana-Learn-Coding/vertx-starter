3eb6b9c1039783f79e6e215902691f1d0662fd65
lana <sitdownrightnow.dev@gmail.com>
2020-06-08 13:19:26 +0700

    add launcher to run directly from ide

--- src/main/java/com/example/vet/VetMain.java
+++ src/main/java/com/example/vet/VetMain.java
@@ -4,13 +4,19 @@ import com.example.vet.database.VetDatabase;
 import com.example.vet.http.VetHttpServer;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Launcher;
 import io.vertx.core.Promise;
 
 public class VetMain extends AbstractVerticle {
+    public static void main(String[] args) {
+        Launcher.executeCommand("run", VetMain.class.getName());
+    }
+
     @Override
     public void start(Promise<Void> promise) {
         Promise<String> vetDBDeployment = Promise.promise();
         vertx.deployVerticle(new VetDatabase(), vetDBDeployment);
+
         vetDBDeployment.future().compose(id -> {
             Promise<String> vetHttpDeployment = Promise.promise();
             // Don't use new (pass instance) if you want to specify the number of instances
