68b1675abfb269d6a7ae3cd2ac6dc3b170bf367c
lana <sitdownrightnow.dev@gmail.com>
2020-06-10 14:48:23 +0700

    validation

---
+++ src/main/java/com/example/vet/validation/UserValidationHandler.java
@@ -0,0 +1,44 @@
+package com.example.vet.validation;
+
+import com.example.vet.model.User;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.ext.web.RoutingContext;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+
+public class UserValidationHandler implements Handler<RoutingContext> {
+
+    @Override
+    public void handle(RoutingContext context) {
+        JsonObject body = context.getBodyAsJson();
+        if (body == null) {
+            return;
+        }
+
+        User user = body.mapTo(User.class);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        if (!violations.isEmpty()) {
+            JsonObject errors = new JsonObject();
+            violations.forEach(violation -> errors.put(violation.getPropertyPath().toString(), violation.getMessage()));
+            context.response()
+                .setStatusCode(400)
+                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .end(errors.encode());
+            return;
+        }
+
+        context.setBody(Buffer.buffer(JsonObject.mapFrom(user).toString()));
+        context.next();
+    }
+}
