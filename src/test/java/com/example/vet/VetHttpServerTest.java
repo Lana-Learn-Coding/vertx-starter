7e7eaa7585a21bee66c6976a14dbbf3ec4df009a
lana <sitdownrightnow.dev@gmail.com>
2020-06-17 18:27:43 +0700

    simple test cases

---
+++ src/test/java/com/example/vet/VetHttpServerTest.java
@@ -0,0 +1,115 @@
+package com.example.vet;
+
+import com.example.vet.model.User;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@RunWith(VertxUnitRunner.class)
+public class VetHttpServerTest {
+    Vertx vertx;
+    int port = 8800;
+
+    @Before
+    public void before(TestContext context) {
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(VetMain.class.getName(), context.asyncAssertSuccess());
+    }
+
+    @After
+    public void after(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void FetchAllUser_ShouldReturnOK(TestContext context) {
+        Async async = context.async();
+        vertx.createHttpClient()
+            .get(port, "localhost", "/users")
+            .handler(res -> {
+                context.assertEquals(200, res.statusCode());
+                async.complete();
+            })
+            .end();
+    }
+
+    @Test
+    public void CreateUser_WithInvalidData_ShouldReturn400(TestContext context) {
+        Async async = context.async();
+        User user = new User();
+        vertx.createHttpClient()
+            .post(port, "localhost", "/users")
+            .handler(res -> {
+                context.assertEquals(400, res.statusCode());
+                async.complete();
+            })
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .setChunked(true)
+            .write(JsonObject.mapFrom(user).toString())
+            .end();
+    }
+
+    @Test
+    public void CreateUser_ValidData_ShouldReturnOk(TestContext context) {
+        Async async = context.async();
+        User user = new User();
+        user.setName("Test Name");
+        user.setActivated(true);
+        Date now = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.YEAR, 2);
+        Date expiredDate = calendar.getTime();
+        user.setActivatedDate(new Date());
+        user.setExpirationDate(expiredDate);
+        user.setEmail("test@test.test");
+        vertx.createHttpClient()
+            .post(port, "localhost", "/users")
+            .handler(res -> {
+                context.assertEquals(200, res.statusCode());
+                async.complete();
+            })
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .setChunked(true)
+            .write(JsonObject.mapFrom(user).toString())
+            .end();
+    }
+
+    @Test
+    public void DeleteUser_ShouldReturnOk(TestContext context) {
+        Async async = context.async();
+        HttpClient client = vertx.createHttpClient();
+        client
+            .get(port, "localhost", "/users")
+            .handler(res -> {
+                context.assertEquals(200, res.statusCode());
+                res.bodyHandler(buffer -> {
+                    JsonArray bodyArray = new JsonArray(buffer);
+                    context.assertNotEquals(0, bodyArray.size());
+
+                    JsonObject item = bodyArray.getJsonObject(0);
+                    context.assertNotNull(item.getString("_id"));
+                    client
+                        .delete(port, "localhost", "/users/" + item.getString("_id"))
+                        .handler(delResponse -> {
+                            context.assertEquals(200, delResponse.statusCode());
+                            async.complete();
+                        })
+                        .end();
+                });
+            })
+            .end();
+    }
+}
